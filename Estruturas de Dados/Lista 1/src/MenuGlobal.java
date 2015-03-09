eade80fdb07680d1708128d76f00f50f6ee57ba4
lockpersi <guuhs@me.com>
2015-03-09 20:09:48 -0300

    unique

---
+++ Estruturas de Dados/Lista 1/src/MenuGlobal.java
@@ -0,0 +1,13 @@
+
+public class MenuGlobal {
+	
+	public static void menuglobal(){
+	System.out.println("****************************");
+	System.out.println("MENU - CADASTRO CLIENTES");
+	System.out.println("1) Cadastrar");
+	System.out.println("2) Pesquisar");
+	System.out.println("3) Relatórios");
+	System.out.println("9) Sair");
+	System.out.print("Digite uma opção: ");
+	}
+}
---
+++ Estruturas de Dados/Lista 1/src/main.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+
+		byte menu;
+		char sairopc;
+		
+		Scanner ler = new Scanner(System.in);
+		
+		MenuGlobal menuglobal = new MenuGlobal();
+		
+		menuglobal.menuglobal();
+		menu = ler.nextByte();
+		
+		//debug menu
+		//System.out.println("Numero do menu:" +  menu);
+		
+		switch(menu){
+		
+		case 1: 
+				System.out.println("Você escolheu Cadastrar Cliente");
+				break;
+		case 2:
+				System.out.println("Voce escolheu Pesquisar Cliente");
+				break;
+		case 3:
+				System.out.println("Voce escolheu Relatórios de Cliente");
+				break;
+		case 9:
+				System.out.println("Voce escolheu sair do programa...");
+				System.out.println("Caso voce realmente deseja sair do "
+						+ "programa digite S caso não digite N: ");
+				
+				Scanner dados = new Scanner(System.in);
+				
+				sairopc = dados.nextLine().charAt(0);
+				
+				switch(sairopc){
+				
+				case 's': case 'S':
+					System.out.println("Saindo do programa...");
+					break;
+				case 'N' : case 'n':
+					main(args);
+					break;
+				default:
+					System.out.println("Erro louco 2z222x2x2x2");
+					break;
+				}
+				
+				break;
+		default:
+			 main(args);
+			 break;
+		
+		}
+		
+	}
+	
+	}
+
