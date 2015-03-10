4b2db7ff78a3226211200c4a2046727a850aecef
lockpersi <guuhs@me.com>
2015-03-10 11:49:46 -0300

    unique

---
+++ Estruturas de Dados/Lista 1/Exer02/src/MenuGlobal.java
@@ -0,0 +1,19 @@
+
+public class MenuGlobal {
+	
+	public static void menuglobal(){
+		
+		System.out.println("****************************");
+		System.out.println("PILHA:");
+		System.out.println("1) Empilhar");
+		System.out.println("2) Desempilhar");
+		System.out.println("3) Topo");
+		System.out.println("4) Imprimir elementos (topo => base)");
+		System.out.println("5) Imprimir índice topo");
+		System.out.println("6) Imprimir número elementos");
+		System.out.println("9) Sair");
+		System.out.println("****************************");
+		
+	}
+
+}
